1689d17e5cfe1bd88479c4eb4b0431fd629012c0
Alvin Leung <alvinleung2009@gmail.com>
2022-08-14 11:43:13 -0700

    fix typo

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import { setupCursor } from "./cusor/cursor";
 window.addEventListener("DOMContentLoaded", () => {
   const [refershCursorTargets, cleanupCursor] = setupCursor();
   //@ts-ignore
-  if (window.daybreak && window.dabyreak.router) {
+  if (window.daybreak && window.daybreak.router) {
     //@ts-ignore
     const router = window.dabyreak.router;
     router.observePageLoad(() => {
