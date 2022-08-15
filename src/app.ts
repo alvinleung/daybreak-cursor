9dda5a9a4c9f0ea5db4f6df5245744c1d036d12a
Alvin Leung <alvinleung2009@gmail.com>
2022-08-14 22:36:54 -0700

    update cursor target

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,18 @@ import { setupCursor } from "./cusor/cursor";
 
 window.addEventListener("DOMContentLoaded", () => {
   const [refershCursorTargets, cleanupCursor] = setupCursor();
+
   //@ts-ignore
-  if (window.daybreak && window.daybreak.router) {
+  window.daybreak = window.daybreak || {}
+
+  //@ts-ignore
+  window.daybreak.cursor = {
+    refershCursorTargets
+  }
+
+
+  //@ts-ignore
+  if (window.daybreak.router) {
     //@ts-ignore
     const router = window.daybreak.router;
     router.observePageLoad(() => {
